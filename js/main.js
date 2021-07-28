c4e91e9310620a08e13a7ac159bd31bd8b3d6edb
casear31 <caesar31c@gmail.com>
2021-07-28 22:38:44 +0300

    addSinglePage

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// const swiper = new Swiper('.swiper-container', {
+//   // Optional parameters
+// //   direction: 'vertical',
+//   loop: true,
+
+  // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+  // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+ var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 6,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        // navigation: {
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        // },
+        thumbs: {
+          swiper: swiper,
+        },
+      });
